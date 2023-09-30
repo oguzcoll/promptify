09590209abe4c06d651a73b31e249dac4f1bbc36
oguzcoll <oguzcolak2000@gmail.com>
2023-10-01 00:23:12 +0300

    get update and delete apis for prompts are completed and update prompt page completed

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,7 +1,13 @@
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+
 import React, { useState } from "react";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+
   const [copied, setCopied] = useState("");
 
   const handleCopy = () => {
@@ -39,6 +45,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             }
             width={12}
             height={12}
+            alt="img_32"
           />
         </div>
       </div>
@@ -48,8 +55,25 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-200 pt-3 ">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
