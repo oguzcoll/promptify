4cd9f37e83cd4e1ed2a1d649654cbabd9790b2e1
oguzcoll <oguzcolak2000@gmail.com>
2023-09-30 15:24:12 +0300

    get request of all post is completed and Promptcard ui also is completed

--- models/prompt.js
+++ models/prompt.js
@@ -7,14 +7,14 @@ const PromptSchema = new Schema({
   },
   prompt: {
     type: String,
-    required: [true, "Prompt is required"],
+    required: [true, "Prompt is required."],
   },
   tag: {
     type: String,
-    required: [true, "Tag is required"],
+    required: [true, "Tag is required."],
   },
 });
 
-const Prompt = model.Prompt || model("Prompt", PromptSchema);
+const Prompt = models.Prompt || model("Prompt", PromptSchema);
 
 export default Prompt;
